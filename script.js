fea4ade2468dff1259efd1baf86777dd99b2ed7b
Guillaume Gonnet <111474584+guillaume-gonnet@users.noreply.github.com>
2023-07-30 20:02:35 -0400

    add button nb squares

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const container = document.getElementById("container");
+const btnSquaresQty = document.getElementById("btnSquaresQty");
+btnSquaresQty.addEventListener('click', promptSquares);
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
-    container.appendChild(div);
+createSquares(256);
+
+function createSquares(nbSquares) {
+    for (let i = 0; i < nbSquares; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        const flexBasisPercent = (100 / Math.sqrt(nbSquares));
+        const flexBasisStr = '' + flexBasisPercent + '%';
+        div.style.flexBasis = flexBasisStr;
+        div.addEventListener('mouseover', () => div.classList.toggle('hovered'));
+        container.appendChild(div);
+    }
+}
+
+function promptSquares() {
+    let nbSquares = 0;
+    while (nbSquares < 1 || nbSquares > 100) {
+        nbSquares = prompt("How many squares do you want per side (Max=100)?");
+        if (nbSquares === null) return;
+    }
+    removeAllChildren(container);
+    createSquares(nbSquares * nbSquares);
+}
+
+function removeAllChildren(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
